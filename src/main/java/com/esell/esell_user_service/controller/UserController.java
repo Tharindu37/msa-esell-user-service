21fc4f593c3d176e34ac0291f52d066a1295d453
Tharindu37 <tharindut520@gmail.com>
2024-12-22 14:33:40 +0530

    password encode

--- src/main/java/com/esell/esell_user_service/controller/UserController.java
+++ src/main/java/com/esell/esell_user_service/controller/UserController.java
@@ -3,6 +3,7 @@ package com.esell.esell_user_service.controller;
 import com.esell.esell_user_service.entity.User;
 import com.esell.esell_user_service.repo.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -12,9 +13,18 @@ import java.util.List;
 public class UserController {
     @Autowired
     private UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public UserController(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
 
     @PostMapping
     public User createUser(@RequestBody User user) {
+        User newUser = new User();
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        user.setEmail(user.getEmail());
+        user.setUsername(user.getUsername());
         return userRepository.save(user);
     }
 
--- src/main/java/com/esell/esell_user_service/entity/User.java
+++ src/main/java/com/esell/esell_user_service/entity/User.java
@@ -1,9 +1,6 @@
 package com.esell.esell_user_service.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -20,7 +17,10 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
+    @Column(nullable = false)
     private String username;
+    @Column(nullable = false)
+    private String password;
+    @Column(nullable = false, unique = true)
     private String email;
 }
